9e2c0d7486db3cddabf3ed62802f1cc663237e39
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-12-25 11:06:32 +0530

    created a seprate component called Cards

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
 import Navbar from './components/Navbar.vue';
 
 
 </script>
 <template>
   <Navbar />
-  <Hero title="Test title" subtitle="This is subtitle"/>
+  <Hero title="Test title" subtitle="Test subtitle"/>
+  <HomeCards />
 </template>
 
 <style scoped></style>
